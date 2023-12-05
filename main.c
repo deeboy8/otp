15fa8b76f7a32d8a7a1703061ccd8d031d714c26
deeboy8 <38823356+deeboy8@users.noreply.github.com>
2023-12-05 18:59:01 +0000

    "removed cl arguments from getopt while loop"

--- main.c
+++ main.c
@@ -2,29 +2,20 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-<<<<<<< HEAD
 extern char *optarg; //???
 
-=======
->>>>>>> refs/remotes/origin/main
 int main(int argc, char* argv[]) {
     int opt;
     FILE* fd;
     int char_count;
+    char* app_name;
 
     while ((opt = getopt(argc, argv, "a:mv")) != -1) { //16 - 38 is setting up state
         switch (opt) {
             case 'a':
                 printf("yup, got a\n");
                 // where the argument for a is held
-                char* app_name = optarg;
-                // validate that app_name is valid --> key, encode, decode
-                if (optarg == "key") {
-                    char_count = atoi(argv[3]) 
-                } 
-                else if (app_name == "encode" || app_name == "decode") {
-                    fd = argv[4]
-                }
+                app_name = optarg;
                 break;
             case 'm':
                 printf("yup, got m\n");
@@ -33,12 +24,21 @@ int main(int argc, char* argv[]) {
                 printf("yup, got v\n");
                 break;
             default:
-                fprintf(stderr, "Usage: %s requires the use of of a keyword: key, encode or decode\n", argv[0]);
-                exit(EXIT_FAILURE);
+                fprintf(stderr, "Usage: %s requires the use of a keyword: key, encode or decode\n", argv[0]);
+                exit(EXIT_FAILURE); //better to return -1?
         }
     }
     //test if app name = key --> go do all the key stuff
     //if app name == encode or decode, then better have access to key file
         // where do we get teh keyfile name (look at getopt)
+    // validate that app_name is valid --> key, encode, decode
+    if (optarg == "key") {
+        char_count = atoi(argv[3]);
+        //generate_key_file(char_count)--> we need plaintext char count? how come up with char_count? 
+    } 
+    else if (app_name == "encode" || app_name == "decode") {
+        fd = argv[4];
+        
+    }
     exit(EXIT_SUCCESS);
 }
