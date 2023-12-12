867ab7b101b2d5c15be56a8459875ecbe3d69c3b
Demitrus Booker <demitrusw.b@gmail.com>
2023-12-11 18:55:15 -0800

    updated the call to the key file from argv[4] to argv[optind]; created stubs for each function call; cleaned up code

--- main.c
+++ main.c
@@ -9,20 +9,33 @@ extern char *optarg; //???
 // macros operate on replacement
 #define IS_STR_EQUAL(str1, str2) (strcmp(str1, str2) == 0)
 
-int main(int argc, char *argv[])
-{
-    int opt;
-    FILE *fd;
-    int char_count;
-    char *app_name;
+void encode(FILE* file) {
+    assert(file);
+    fprintf(stderr, "encoding message");
+    return;
+}
 
-    while ((opt = getopt(argc, argv, "a:mv")) != -1)
-    { // 16 - 38 is setting up state
-        switch (opt)
-        {
+void decode(FILE* file) {
+    assert(file);
+    fprintf(stderr, "decoding message");
+    return;
+}
+void generate_key(int count) {
+    assert(count != 0);
+    fprintf(stderr, "generating randomized set of characters");
+    return; //vs SUCCESS vs exit(EXIT_SUCCESS)
+}
+
+int main(int argc, char *argv[]) {
+    int opt;
+    FILE* fd;
+    int char_count;
+    char* app_name;
+
+    while ((opt = getopt(argc, argv, "a:mv")) != -1) { 
+        switch (opt) {
         case 'a':
-            printf("yup, got a\n"); // fprintf to stderr  to seperate from output of program !!!!!!!!!!!!!
-            // where the argument for a is held
+            printf("yup, got a\n"); // fprintf to stderr  to seperate from output of program !!!!!!!!!!
             app_name = optarg;
             break;
         case 'm':
@@ -36,31 +49,21 @@ int main(int argc, char *argv[])
             exit(EXIT_FAILURE); // better to return -1?
         }
     }
-    // test if app name = key --> go do all the key stuff
-    // if app name == encode or decode, then better have access to key file
-    //  where do we get teh keyfile name (look at getopt)
-    if (IS_STR_EQUAL("key", app_name))
-    {
-        char_count = atoi(argv[3]); //-----> optind!?
-        assert(char_count != 0);
-        // generate_key_file(char_count)--> we need plaintext char count? how come up with char_count?
-    }
-    else 
-    {
-        fd = fopen(argv[4], "r"); // key.key
-        if (IS_STR_EQUAL("encode", app_name))
-        {
-            // code stub for encode message, implementation of encode mesg 
-            // encode_message()
+
+    if (IS_STR_EQUAL("key", app_name)) {
+        char_count = atoi(argv[optind]);
+        assert(char_count >= 0);
+        generate_key(char_count); // generate_key_file(char_count)--> we need plaintext char count? how come up with char_count?
+    } else {
+        fd = fopen(argv[optind], "r"); // key.key
+        if (IS_STR_EQUAL("encode", app_name)) {
+            encode(fd); //do I want to pass around a file descriptor????
+        }
+        else if (IS_STR_EQUAL("decode", app_name)) {
+            decode(fd);
         }
 
-        else if (IS_STR_EQUAL("decode", app_name))
-        {
-            // fd = fopen(argv[4], "r"); // key.key
-            // decode_message()
-        }
-
-        fclose(fd); // BUG?
+        fclose(fd); 
     }
 
     exit(EXIT_SUCCESS);
