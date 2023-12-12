e7893cc6ab548c2ad9082a21b1c7380cf90b194f
Demitrus Booker <demitrusw.b@gmail.com>
2023-12-11 20:41:06 -0800

    added to do points and CL UTs

--- main.c
+++ main.c
@@ -9,18 +9,18 @@ extern char *optarg; //???
 // macros operate on replacement
 #define IS_STR_EQUAL(str1, str2) (strcmp(str1, str2) == 0)
 
-void encode(FILE* file) {
+void encode(FILE* file) { //TODO
     assert(file);
     fprintf(stderr, "encoding message");
     return;
 }
 
-void decode(FILE* file) {
+void decode(FILE* file) { //TODO
     assert(file);
     fprintf(stderr, "decoding message");
     return;
 }
-void generate_key(int count) {
+void generate_key(int count) { //TODO
     assert(count != 0);
     fprintf(stderr, "generating randomized set of characters");
     return; //vs SUCCESS vs exit(EXIT_SUCCESS)
@@ -50,14 +50,14 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    if (IS_STR_EQUAL("key", app_name)) {
+    if ((strcmp("key", app_name) == 0)) { //(IS_STR_EQUAL("key", app_name)) {
         char_count = atoi(argv[optind]);
         assert(char_count >= 0);
         generate_key(char_count); // generate_key_file(char_count)--> we need plaintext char count? how come up with char_count?
     } else {
         fd = fopen(argv[optind], "r"); // key.key
         if (IS_STR_EQUAL("encode", app_name)) {
-            encode(fd); //do I want to pass around a file descriptor????
+            encode(fd); //do I want to pass around a file descriptor???? TODO
         }
         else if (IS_STR_EQUAL("decode", app_name)) {
             decode(fd);
