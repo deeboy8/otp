39beb88b0f86e097acd3ada5ec5c2a488829a4c4
Demitrus Booker <demitrusw.b@gmail.com>
2023-12-14 06:48:19 -0800

    pushing wyzant notes in misc/

--- misc/Wyzant-lesson-6-11-2023.c
+++ misc/Wyzant-lesson-6-11-2023.c
@@ -1,5 +1,6 @@
-﻿// as is
-keygen 256 > key.key
+﻿/*
+// as is
+//keygen 256 > key.key
 encode plain.txt key.key // cipher.txt
 decode cipher.txt key.key > new_plain.txt
 diff plain.txt new_plain.txt // nothing
@@ -9,7 +10,7 @@ keygen 512 > key.key
 // encode w/client & server
 cat plain.txt | encode_c key.key > { key, plain_text }
 encode_s key.key < { key, plain_text } > cipher.txt
-// encode w/c/s pipeline
+// encode w/c/s pipeline//
 cat plain.txt | encode_c key.key | encode_s > cipher.txt
 
 cat cipher.txt | decode_c key.key > { key, plain_text }
@@ -25,4 +26,6 @@ cat plain.txt | encode_c key.key | encode_s | decode_c key.key | decode_s | diff
 ./otp -a decode key.key ---> SUCCESS 
 ./otp encode/decode key.key ---> error ---> usage error
 ./otp -a encode/decode ----> error ---> "usage: ./otp requires key file"
- 
+ */
+
+encode key.key < plaintext.txt > ciphertext.txt
